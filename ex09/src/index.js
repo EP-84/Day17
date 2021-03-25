cb92bde35d03305a4007ae4474eda086942f9f2e
EP-84 <edmir.prasovic@gmail.com>
2021-03-25 09:38:50 +0100

    v1

---
+++ ex09/src/index.js
@@ -0,0 +1,40 @@
+const _ = require('lodash');
+
+var users = [
+    { 
+      firstName: "John",
+      lastName: "Doe",
+      age: 24,
+      gender: male
+
+    },
+
+    { 
+        firstName: "Jane",
+        lastName: "Doe",
+        age: 5,
+        gender: "female"
+  
+      },
+
+      { 
+        firstName: "Jim",
+        lastName: "Carrey",
+        age: 54,
+        gender: "male"
+  
+      },
+      { 
+        firstName: "Kate",
+        lastName: "Winstel",
+        age: 40,
+        gender: "female"
+  
+      },
+];
+
+function getUsers() {
+
+
+    
+}
